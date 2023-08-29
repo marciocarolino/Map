26deb1673f4bbb01a5af9c5cb843b99a14953239
Marcio Carolino <marciocarolino@gmail.com>
2023-08-29 14:19:11 -0300

    ainda continuando map

--- src/main/java/org/example/EstoqueProdutos.java
+++ src/main/java/org/example/EstoqueProdutos.java
@@ -1,8 +1,49 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProdutos {
 
 
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
     public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProduto() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+
+        double valortotalEstoque = 0;
+
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                valortotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+
+        return valortotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if (p.getPreco() > maiorPreco) {
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,34 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        AgendaContatos agendaContatos = new AgendaContatos();
+//        AgendaContatos agendaContatos = new AgendaContatos();
+//
+//        agendaContatos.adicionarContato("Marcio", 123456);
+//        agendaContatos.adicionarContato("Marcio", 5665);
+//        agendaContatos.adicionarContato("Marcio DIO", 111111111);
+//        agendaContatos.adicionarContato("Marcio Carolino", 45689654);
+//        agendaContatos.adicionarContato("Maria Silva", 111111111);
+//        agendaContatos.adicionarContato("Marcio", 4444444);
+//
+//        // agendaContatos.exibirContatos();
+//
+//        agendaContatos.removerContato("Maria Silva");
+//        agendaContatos.exibirContatos();
+//
+//        System.out.println("O número é.: " + agendaContatos.pesquisarPorNome("Marcio DIO"));
 
-        agendaContatos.adicionarContato("Marcio", 123456);
-        agendaContatos.adicionarContato("Marcio", 5665);
-        agendaContatos.adicionarContato("Marcio DIO", 111111111);
-        agendaContatos.adicionarContato("Marcio Carolino", 45689654);
-        agendaContatos.adicionarContato("Maria Silva", 111111111);
-        agendaContatos.adicionarContato("Marcio", 4444444);
 
-        // agendaContatos.exibirContatos();
+        EstoqueProdutos estoqueProdutos = new EstoqueProdutos();
+//        estoqueProdutos.exibirProduto();
 
-        agendaContatos.removerContato("Maria Silva");
-        agendaContatos.exibirContatos();
+        estoqueProdutos.adicionarProduto(1L, "Produto A", 10, 5.0);
+        estoqueProdutos.adicionarProduto(2L, "Produto B", 5, 10.0);
+        estoqueProdutos.adicionarProduto(3L, "Produto C", 2, 15.0);
 
-        System.out.println("O número é.: " + agendaContatos.pesquisarPorNome("Marcio DIO"));
+        estoqueProdutos.exibirProduto();
 
+        System.out.println("Valor total do estoque: R$ " + estoqueProdutos.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: " + estoqueProdutos.obterProdutoMaisCaro());
 
     }
 }
