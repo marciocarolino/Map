4643a091295a0f20db46f14aa7fe4fc2c795ce1d
Marcio Carolino <marciocarolino@gmail.com>
2023-08-29 11:37:13 -0300

    continue project

---
+++ src/main/java/org/example/EstoqueProdutos.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class EstoqueProdutos {
+
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Marcio", 123456);
+        agendaContatos.adicionarContato("Marcio", 5665);
+        agendaContatos.adicionarContato("Marcio DIO", 111111111);
+        agendaContatos.adicionarContato("Marcio Carolino", 45689654);
+        agendaContatos.adicionarContato("Maria Silva", 111111111);
+        agendaContatos.adicionarContato("Marcio", 4444444);
+
+        // agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria Silva");
+        agendaContatos.exibirContatos();
+
+        System.out.println("O número é.: " + agendaContatos.pesquisarPorNome("Marcio DIO"));
+
+
     }
 }
